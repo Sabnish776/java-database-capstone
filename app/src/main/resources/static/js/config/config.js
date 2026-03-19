0f0145b2cc7faa2725b39264c5e70a44e08e46c5
Sabnish776 <sabnish1776@gmail.com>
2026-03-19 18:12:30 +0530

    Changed base URL

--- app/src/main/resources/static/js/config/config.js
+++ app/src/main/resources/static/js/config/config.js
@@ -11,4 +11,5 @@
  *   fetch(`${API_BASE_URL}/api/appointments`)
  */
 
-export const API_BASE_URL = "http://localhost:8080";
+// export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://cms-tja7.onrender.com";
